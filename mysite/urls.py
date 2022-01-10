5de41284a0cda68e7789a805417bfeb4d8fe5eab
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2022-01-10 17:22:49 +0500

    gg

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,11 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
 from django.conf.urls.i18n import i18n_patterns
+from django.urls import path,include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+
 ]
 
 urlpatterns += i18n_patterns(path('',include('myapp.urls')),prefix_default_language=False)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
